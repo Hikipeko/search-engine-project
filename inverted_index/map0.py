bb666fc02810d2b13d1de9b55383a2ff46b50ec0
joelchen <joelchen@umich.edu>
2022-11-23 15:03:19 -0500

    add map0, reduce 0

--- inverted_index/map0.py
+++ inverted_index/map0.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python
 """Map 0."""
+
+import csv
+import sys
+
+csv.field_size_limit(sys.maxsize)
+data = sys.stdin.readlines()
+print(f"D\t{len(data)}")
--- inverted_index/reduce0.py
+++ inverted_index/reduce0.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
 """Reduce 0."""
+
+import sys
+
+doc_count = 0
+data = sys.stdin.readlines()
+for line in data:
+    count = line.partition("\t")[2]
+    if not count:
+        continue
+    doc_count += int(count)
+print(doc_count)
