c49164fe405118667c063be72604807242fd5d8d
hyyy <hyyy@umich.edu>
2022-12-06 11:50:50 -0500

    Fix bugs for search server; Format coding styles.

--- search_server/search/views/__init__.py
+++ search_server/search/views/__init__.py
@@ -1,1 +1,4 @@
+"""Init for views."""
+
+
 from search.views.index import show_index
--- search_server/search/views/index.py
+++ search_server/search/views/index.py
@@ -1,16 +1,17 @@
 """
 ask485 index (main) view.
+
 URLs include:
 /
 """
-from datetime import datetime
-import flask
-import search
-import requests
 import heapq
 import threading
 import time
 import itertools
+import search
+import requests
+import flask
+
 
 @search.app.route('/', methods=['GET'])
 def show_index():
@@ -24,57 +25,56 @@ def show_index():
     num_thread = len(search.config.SEARCH_INDEX_SEGMENT_API_URLS)
     results = [[]] * num_thread
     threads = []
-
     for idx in range(num_thread):
         url = search.config.SEARCH_INDEX_SEGMENT_API_URLS[idx]
         payload = {'q': query, 'w': weight}
-        thread = threading.Thread(target=reqIndexServer, args=(idx, url, payload, results))
+        thread = threading.Thread(
+            target=req_index_server, args=(idx, url, payload, results))
         threads.append(thread)
         thread.start()
-    # for url in search.config.SEARCH_INDEX_SEGMENT_API_URLS:
-    #     payload = {'q': quest, 'w': weight}
-    #     thread = threading.Thread(target=reqIndexServer, args=(url,payload,results,))
-    #     threads.append(thread)
-    #     thread.start()
-        # print(resp.json)
-        # heapq.merge(results,resp.json)
-        
     for thread in threads:
         thread.join()
     context = {}
-    num_results = sum([len(r) for r in results])
+    num_results = sum(len(r) for r in results)
     if num_results > 0:
-        merged_result = heapq.merge(*results, key = lambda x: x['score'], reverse=True)
+        merged_result = heapq.merge(
+            *results, key=lambda x: x['score'], reverse=True)
         result_list = []
         first_results = itertools.islice(merged_result, min(num_results, 10))
-        result_list = list(map(lambda x: fetchDocFromId(connection=connection, docid=x['docid']), first_results))
+        result_list = list(map(lambda x: fetch_doc_from_id(
+            connection=connection,
+            docid=x['docid']),
+            first_results))
         context['results'] = result_list
         # print("context['results']")
         # print(context['results'])
     else:
         # no index result found for this query
-        pass 
+        pass
     context['text'] = query
     # print(context['text'])
     context['weight'] = weight
     return flask.render_template("index.html", **context)
 
 
-def reqIndexServer(tid, url, payload, results):
-    try:
-        resp = requests.get(url, params=payload)
-    except Exception as e:
-        print("######### Error!")
-        print(e)
-        exit(-1)
+def req_index_server(tid, url, payload, results):
+    """Thread to send GET requests to index server."""
+    # try:
+    resp = requests.get(url, params=payload, timeout=10)
+    # except Exception as e:
+    #     print("######### Error!")
+    #     print(e)
+    #     exit(-1)
     results[tid] = resp.json()['hits']
     print(f"thread-{tid} done: {len(results[tid])}")
     time.sleep(0.1)
 
-def fetchDocFromId(connection, docid):
+
+def fetch_doc_from_id(connection, docid):
+    """Fetch the document from docid."""
     doc = connection.execute(
-    'SELECT * FROM Documents '
-    'WHERE docid = ?',
-    (docid, )
+        'SELECT * FROM Documents '
+        'WHERE docid = ?',
+        (docid, )
     ).fetchone()
     return doc
